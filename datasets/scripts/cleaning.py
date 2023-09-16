9eb9e2283b1c1c9e3b5d622420279ee104ad4d0c
Alyssasorensen <-m>
2023-09-16 02:00:17 +0000

    added information to the cleaning.py file and removed it from the eda.py file

--- datasets/scripts/cleaning.py
+++ datasets/scripts/cleaning.py
@@ -1,31 +1,88 @@
-### Loading in packages 
-import pandas as pd 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-### Load in data 
-df_patients = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/datasci_3_eda/main/datasets/processed/Hospitalization_Discharge_Rates.csv')
-df_patients['patient_id'].nunique()
+## loading the clean .csv file - patients_cleaned.csv
 
-### Look for missingness
-df_patients.sample(10)
-df_patients.isnull()
-df_patients.isnull().sum()
+df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/datasci_3_eda/main/datasets/processed/Hospitalization_Discharge_Rates.csv')
 
-percentmissing = pd.DataFrame(df_patients.isnull().sum()).reset_index()
-percentmissing.rename(columns = {'index': 'variable', 0:'count_missing'}, inplace = true)
-percentmissing['missing_percent'] = percentmissing['count_missing']/(df_patients.shape[0]) * 100 
+df.columns
 
-### Look for outliers 
+# Display the first few rows of the dataset
+df.head()
 
-df_patients.describe()
+# Calculate measures of central tendency
+mean = df['Diabetes'].mean()
+median = df['Diabetes'].median()
+mode = df['Diabetes'].mode().values[0]
 
-### Variables that we have identified that need conversion: year_of_birth, month_year_death
-### If missing values you'll get an error 
-df_patients['year_of_birth'] = df_patients['year_of_birth'].astype(float).apply(np, round)
-df_patients['month_year_death'] = df_patients['month_year_death'].str.replace('.0', '')
+# Calculate measures of central tendency
+mean = df['Hypertensi'].mean()
+median = df['Hypertensi'].median()
+mode = df['Hypertensi'].mode().values[0]
 
-### Univariate descriptives: (descriptives, frequencies, histograms, etc.)
+# Calculate measures of spread
+range_val = df['Diabetes'].max() - df['Diabetes'].min()
+variance = df['Diabetes'].var()
+std_deviation = df['Diabetes'].std()
+iqr = df['Diabetes'].quantile(0.75) - df['Diabetes'].quantile(0.25)
 
+# Calculate measures of spread
+range_val = df['Hypertensi'].max() - df['Hypertensi'].min()
+variance = df['Hypertensi'].var()
+std_deviation = df['Hypertensi'].std()
+iqr = df['Hypertensi'].quantile(0.75) - df['Hypertensi'].quantile(0.25)
 
-### Bivariate descriptives: (correlations, covariances)
+# Print the results
+print("Measures of Central Tendency:")
+print(f"Mean: {mean}")
+print(f"Median: {median}")
+print(f"Mode: {mode}")
+print("\nMeasures of Spread:")
+print(f"Range: {range_val}")
+print(f"Variance: {variance}")
+print(f"Standard Deviation: {std_deviation}")
+print(f"IQR (Interquartile Range): {iqr}")
 
-df_patients.to_csv('https://raw.githubusercontent.com/Alyssasorensen/HHA507class3/main/patient.csv')
+# Print the results
+print("Measures of Central Tendency:")
+print(f"Mean: {mean}")
+print(f"Median: {median}")
+print(f"Mode: {mode}")
+print("\nMeasures of Spread:")
+print(f"Range: {range_val}")
+print(f"Variance: {variance}")
+print(f"Standard Deviation: {std_deviation}")
+print(f"IQR (Interquartile Range): {iqr}")
+
+# Create a histogram
+plt.figure(figsize=(8, 6))
+plt.hist(df['Diabetes'], bins=20, color='skyblue', edgecolor='black')
+plt.title('Distribution of Diabetes')
+plt.xlabel('Diabetes')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
+
+# Create a histogram
+plt.figure(figsize=(8, 6))
+plt.hist(df['Hypertensi'], bins=20, color='navy', edgecolor='black')
+plt.title('Distribution of Hypertensi')
+plt.xlabel('Hypertensi')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Scatter plot for Diabetes vs. Hypertension
+plt.figure(figsize=(8, 6))
+plt.scatter(df['Diabetes'], df['Hypertensi'], alpha=0.5, color='blue')
+plt.title('Scatter Plot: Diabetes vs. Hypertensi')
+plt.xlabel('Diabetes')
+plt.ylabel('Hypertensi')
+plt.grid(True)
+plt.show()
