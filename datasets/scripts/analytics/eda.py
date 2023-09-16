623554d42d7ffe22f53eeb438fe5e06637671e42
Alyssasorensen <-m>
2023-09-16 01:13:56 +0000

    updated the eda.py file

--- datasets/scripts/analytics/eda.py
+++ datasets/scripts/analytics/eda.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 ## loading the clean .csv file - patients_cleaned.csv
 
 df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/datasci_3_eda/main/datasets/processed/Hospitalization_Discharge_Rates.csv')
+
 df.columns
